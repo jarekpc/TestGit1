b2d837ba5b0cf9ee5a8cbdbeb0ed2286abcf59f7
jarekpc <jarekpc@o2.pl>
2019-01-14 09:53:33 +0100

    added a new format for file Main

--- src/main/java/com/jarek/Main.java
+++ src/main/java/com/jarek/Main.java
@@ -5,5 +5,7 @@ public class Main {
     public static void main(String[] args) {
         double a = 12.2;
         System.out.format("To jest wartosc %f\n", a);
+        long n = 45012;
+        System.out.format("%d%n",n);
     }
 }
